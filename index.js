cf26af64829ddd59b5db5415cecacc9187b70254
Griffin Telljohann <g.telljohann@gmail.com>
2017-08-10 10:45:07 -0400

    updating version, exporting more stuff

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ module.exports = require('./dmv');
 
 module.exports.canMixin = require('./canMixin');
 module.exports.angularPlugin = require('./angularPlugin');
+module.exports.expressMiddleware = require('./expressMiddleware');
+module.exports.mongoosePlugin = require('./mongoosePlugin');
+module.exports.permissionSchema = require('./permissionSchema')
